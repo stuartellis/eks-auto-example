4ee39f438bf5870a16e84fce5b0f1407061feae8
Stuart Ellis <stuart@stuartellis.name>
2025-04-07 00:27:25 +0100

    fix(docs): Fix building

--- tf/definitions/template/locals.tf
+++ tf/definitions/template/locals.tf
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 #
-# SPDX-FileCopyrightText: 2025-present Stuart Ellis <stuart@stuartellis.name>
-#
-# SPDX-License-Identifier: MIT
-#
+
+locals {
+  standard_prefix = "${var.environment_name}-${var.variant}-${var.stack_name}"
+}
