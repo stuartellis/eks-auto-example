f9d441c11aa95d8021f05abf3a9b1a4057b35a06
Stuart Ellis <stuart@stuartellis.name>
2025-04-20 23:46:38 +0100

    feat(tf): Disable write for deploy key

--- tf/definitions/amc-gitlab/gitlab_amc_deploy_key.tf
+++ tf/definitions/amc-gitlab/gitlab_amc_deploy_key.tf
@@ -12,5 +12,5 @@ resource "gitlab_deploy_key" "amc_flux" {
   project  = data.gitlab_project.this.path_with_namespace
   title    = local.amc_gitlab_deploy_key_secret_name
   key      = tls_private_key.amc_flux.public_key_openssh
-  can_push = true
+  can_push = false
 }
